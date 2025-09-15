ac8ad04b0810f65015db996dca019dccf552f217
Luis Antonio <14luisdias@gmail.com>
2025-09-15 13:00:53 -0500

    avance até auça 8 -> Mostrando os dados formatados corretamente

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -20,15 +20,45 @@
         </div>
     </nav>
     <div class="container mx-auto">
-        <h1 class="text-3xl font-bold underline">
-            Hello world!
+        <h1 class="text-4xl font-bold text-center my-4">
+            Lista de Clientes
         </h1>
-        @foreach ($clientes as $cliente)
-            <strong> Nome: </strong> {{ $cliente->nome }} |
-            <strong> Endereço: </strong> {{ $cliente->endereco }} |
-            <strong> Descrição: </strong> {{ $cliente->descricao }}
-            <br>
-        @endforeach
+
+
+        <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                    <tr>
+                        <th scope="col" class="px-6 py-3">
+                            Nome
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Endereço
+                        </th>
+                        <th scope="col" class="px-6 py-3">
+                            Descrição
+                        </th>
+                    </tr>
+                </thead>
+
+                @foreach ($clientes as $cliente)
+                    <tbody>
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                {{ $cliente->nome }}
+                            </th>
+                            <td class="px-6 py-4">
+                                {{ $cliente->endereco }}
+                            </td>
+                            <td class="px-6 py-4">
+                                {{ $cliente->descricao }}
+                            </td>
+                        </tr>
+                    </tbody>
+                @endforeach
+            </table>
+        </div>
+        
     </div>
 
 </body>
